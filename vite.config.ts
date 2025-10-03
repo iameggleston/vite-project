fba8b863cdd2185b3dbc593eb367a1efdb4fc074
takayoshi ando <an3b8n6mpwqnjpdo@gmail.com>
2025-10-03 17:01:51 +0900

    chore: update TypeScript and Vite configurations to include additional paths and aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      '~': resolve(__dirname, '.'),
       '@': resolve(__dirname, './src'),
     },
   },
@@ -23,4 +24,4 @@ export default defineConfig({
       localsConvention: 'camelCase',
     },
   },
-})
+});
